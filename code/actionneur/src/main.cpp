43346976d0977d90271b2a0afc1103628959c3d8
Samy Rahmani <samy.osmium@gmail.com>
2026-07-17 10:46:43 -0400

    feat: added dynamic wifi and persistent storage

--- code/actionneur/src/main.cpp
+++ code/actionneur/src/main.cpp
@@ -1,17 +1,24 @@
 //=============================================================================================================================
 //=============================================================================================================================
-//! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! !
+//! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! !
+//! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! !
 
-// WARNING: If you plan to use this code with more than 1 valve, you need to have a dedicated 5V power supply for the valves
+// WARNING: If you plan to use this code with more than 1 valve, you need to
+// have a dedicated 5V power supply for the valves
 
-//! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! !
+//! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! !
+//! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! !
 //=============================================================================================================================
 //=============================================================================================================================
 
-
-#include <AliotObject.h>
-#include <cstring>
 #include "SECRET.h"
+#include <AliotObject.h>
+#include <ArduinoJson.h>
+#include <cstring>
+
+#ifdef ESP32
+#include <Preferences.h>
+#endif
 
 long lastMillis = 0;
 long delayMillis = 500; // 0.5 second
@@ -22,201 +29,258 @@ bool isSetupDone = false;
 const int BAUD_RATE = 115200;
 
 // Path for the document
-const char* docPath = "/doc/";
+const char *docPath = "/doc/";
 
 // Type def for the valves
 typedef struct {
-    const char* actionId;
-    const uint8_t pin;
-    const bool isPinInverted;
+  const char *actionId;
+  const uint8_t pin;
+  const bool isPinInverted;
 } Valve;
 
 // Array of valves
 const Valve valves[] = // Changed to an array
-    {
-        {
-            .actionId = "valve_1",
-            .pin = 25,
-            .isPinInverted = false
-        },
-        {
-            .actionId = "valve_2",
-            .pin = 26,
-            .isPinInverted = false
-        },
-                {
-            .actionId = "valve_3",
-            .pin = 27,
-            .isPinInverted = false
-        },
-        {
-            .actionId = "valve_4",
-            .pin = 14,
-            .isPinInverted = false
-        },
-        {
-            .actionId = "valve_5",
-            .pin = 18,
-            .isPinInverted = false
-        }
-    };
+    {{.actionId = "valve_1", .pin = 25, .isPinInverted = false},
+     {.actionId = "valve_2", .pin = 26, .isPinInverted = false},
+     {.actionId = "valve_3", .pin = 27, .isPinInverted = false},
+     {.actionId = "valve_4", .pin = 14, .isPinInverted = false},
+     {.actionId = "valve_5", .pin = 18, .isPinInverted = false}};
 
-// Crate AliotObject instance 
+// Crate AliotObject instance
 AliotObject aliotObj = AliotObject();
 
+// NVS storage for remotely-updated wifi credentials
+#ifdef ESP32
+Preferences preferences;
+#endif
+const char *WIFI_PREF_NAMESPACE = "wifi_cfg";
+const char *WIFI_PREF_KEY_SSID = "ssid";
+const char *WIFI_PREF_KEY_PASS = "password";
+
+// Currently active wifi credentials (loaded from NVS, falling back to
+// SECRET.h on first boot)
+String currentSsid;
+String currentPassword;
+
 // Convert string to a bool
-bool stringToBool(const char* str) {
-    return std::strcmp(str, "true") == 0;
-}
+bool stringToBool(const char *str) { return std::strcmp(str, "true") == 0; }
 
-void setValveState(const Valve& valve, bool state) {
-    if (valve.isPinInverted) {
-        digitalWrite(valve.pin, !state);
-        aliotObj.updateDoc(
-            createDict<bool>(
-                Pair<bool>((std::string(docPath) + valve.actionId).c_str(), !state)
-            )
-        );
-    } else {
-        digitalWrite(valve.pin, state);
-        aliotObj.updateDoc(
-            createDict<bool>(
-                Pair<bool>((std::string(docPath) + valve.actionId).c_str(), state)
-            )
-        );
-    }
+void setValveState(const Valve &valve, bool state) {
+  if (valve.isPinInverted) {
+    digitalWrite(valve.pin, !state);
+    aliotObj.updateDoc(createDict<bool>(
+        Pair<bool>((std::string(docPath) + valve.actionId).c_str(), !state)));
+  } else {
+    digitalWrite(valve.pin, state);
+    aliotObj.updateDoc(createDict<bool>(
+        Pair<bool>((std::string(docPath) + valve.actionId).c_str(), state)));
+  }
 }
-
 
 // Function to be called when valve 1 state is changed on ALIVEcode
-bool callbackValve1(const char* data) {
-    
-    Serial.print("New valve 1 state:");
-    Serial.println(data);
+bool callbackValve1(const char *data) {
 
-    // Set valve 1 state
-    setValveState(valves[0], stringToBool(data));
+  Serial.print("New valve 1 state:");
+  Serial.println(data);
 
-    return true;
+  // Set valve 1 state
+  setValveState(valves[0], stringToBool(data));
+
+  return true;
 }
 
 // Function to be called when valve 2 state is changed on ALIVEcode
-bool callbackValve2(const char* data) {
-    Serial.print("New valve 2 state:");
-    Serial.println(data);
+bool callbackValve2(const char *data) {
+  Serial.print("New valve 2 state:");
+  Serial.println(data);
 
-    // Set valve 2 state
-    setValveState(valves[1], stringToBool(data));
+  // Set valve 2 state
+  setValveState(valves[1], stringToBool(data));
 
-    return true;
+  return true;
 }
 
 // Function to be called when valve 3 state is changed on ALIVEcode
-bool callbackValve3(const char* data) {
-    Serial.print("New valve 3 state:");
-    Serial.println(data);
+bool callbackValve3(const char *data) {
+  Serial.print("New valve 3 state:");
+  Serial.println(data);
 
-    // Set valve 3 state
-    setValveState(valves[2], stringToBool(data));
+  // Set valve 3 state
+  setValveState(valves[2], stringToBool(data));
 
-    return true;
+  return true;
 }
 
 // Function to be called when valve 4 state is changed on ALIVEcode
-bool callbackValve4(const char* data) {
-    Serial.print("New valve 4 state:");
-    Serial.println(data);
+bool callbackValve4(const char *data) {
+  Serial.print("New valve 4 state:");
+  Serial.println(data);
 
-    // Set valve 4 state
-    setValveState(valves[3], stringToBool(data));
+  // Set valve 4 state
+  setValveState(valves[3], stringToBool(data));
 
-    return true;
+  return true;
 }
 
 // Function to be called when valve 5 state is changed on ALIVEcode
-bool callbackValve5(const char* data) {
-    Serial.print("New valve 5 state:");
-    Serial.println(data);
+bool callbackValve5(const char *data) {
+  Serial.print("New valve 5 state:");
+  Serial.println(data);
 
-    // Set valve 5 state
-    setValveState(valves[4], stringToBool(data));
+  // Set valve 5 state
+  setValveState(valves[4], stringToBool(data));
 
-    return true;
+  return true;
+}
+
+// Load wifi credentials from NVS, falling back to the hardcoded SECRET.h
+void loadWifiCredentials() {
+#ifdef ESP32
+  preferences.begin(WIFI_PREF_NAMESPACE, true);
+  currentSsid = preferences.getString(WIFI_PREF_KEY_SSID, SSID);
+  currentPassword = preferences.getString(WIFI_PREF_KEY_PASS, PASSWORD);
+  preferences.end();
+#else
+  currentSsid = SSID;
+  currentPassword = PASSWORD;
+#endif
+  Serial.print("Using wifi SSID: ");
+  Serial.println(currentSsid);
+}
+
+// Persist new wifi credentials to NVS so they survive reboot. No-op (aside
+// from logging) on platforms without Preferences support.
+void saveWifiCredentials(const String &ssid, const String &password) {
+#ifdef ESP32
+  preferences.begin(WIFI_PREF_NAMESPACE, false);
+  preferences.putString(WIFI_PREF_KEY_SSID, ssid);
+  preferences.putString(WIFI_PREF_KEY_PASS, password);
+  preferences.end();
+  Serial.println("Wifi credentials saved to NVS");
+#else
+  Serial.println("Preferences not available on this platform; wifi credentials "
+                 "not persisted");
+#endif
+}
+
+// Function to be called when new wifi credentials are pushed from ALIVEcode.
+// Expects `data` to be a JSON string: {"ssid": "...", "password": "..."}
+bool callbackWifiConfig(const char *data) {
+  Serial.print("New wifi config received: ");
+  Serial.println(data);
+
+  StaticJsonDocument<256> doc;
+  DeserializationError err = deserializeJson(doc, data);
+  if (err) {
+    Serial.print("Failed to parse wifi_config payload: ");
+    Serial.println(err.c_str());
+    return false;
+  }
+  if (!doc.containsKey("ssid") || !doc.containsKey("password")) {
+    Serial.println("wifi_config payload missing 'ssid' or 'password' key");
+    return false;
+  }
+
+  currentSsid = doc["ssid"].as<String>();
+  currentPassword = doc["password"].as<String>();
+
+  saveWifiCredentials(currentSsid, currentPassword);
+
+  aliotObj.setupConfig(AUTH_TOKEN, OBJECT_ID, currentSsid.c_str(),
+                       currentPassword.c_str());
+  // Force a reconnect; aliotObj.loop()'s existing self-healing path picks up
+  // the new config on its next iteration
+  WiFi.disconnect();
+
+  Serial.println("Switching to new wifi network...");
+  return true;
 }
 
 // Update all valves from ALIVEcode
 void updateValveFromServer() {
-    for (int i = 0; i < sizeof(valves) / sizeof(valves[0]); i++) {  // Updated loop condition
-        // Get valve state from ALIVEcode
-        String res = aliotObj.getDoc(String(docPath) + String(valves[i].actionId));
-        bool state = stringToBool(res.c_str());
+  for (int i = 0; i < sizeof(valves) / sizeof(valves[0]);
+       i++) { // Updated loop condition
+    // Get valve state from ALIVEcode
+    String res = aliotObj.getDoc(String(docPath) + String(valves[i].actionId));
+    bool state = stringToBool(res.c_str());
 
-        // Set valve state
-        setValveState(valves[i], state);
+    // Set valve state
+    setValveState(valves[i], state);
 
-        Serial.println(valves[i].actionId + String(" state updated (state: ") + res + ")");
+    Serial.println(valves[i].actionId + String(" state updated (state: ") +
+                   res + ")");
 
-        // sleep for 0.5 sec
-        lastMillis = millis();
-        while (millis() - lastMillis < delayMillis) {
-            aliotObj.loop();
-        }
+    // sleep for 0.5 sec
+    lastMillis = millis();
+    while (millis() - lastMillis < delayMillis) {
+      aliotObj.loop();
     }
+  }
 }
 
 // Called when the wifi is connected again
 void onReconnect() {
-    Serial.println("Reconnected to ALIVEcode");
-    updateValveFromServer();
+  Serial.println("Reconnected to ALIVEcode");
+  updateValveFromServer();
 }
 
 void setup() {
-    Serial.begin(BAUD_RATE);
+  Serial.begin(BAUD_RATE);
 
-    Serial.println("Starting...");
+  Serial.println("Starting...");
 
-    aliotObj.setupConfig(AUTH_TOKEN, OBJECT_ID, SSID, PASSWORD);
+  loadWifiCredentials();
+  aliotObj.setupConfig(AUTH_TOKEN, OBJECT_ID, currentSsid.c_str(),
+                       currentPassword.c_str());
 
-    aliotObj.setReconnectCallback(onReconnect);
+  aliotObj.setReconnectCallback(onReconnect);
 
-    // Start connection process and listen for events
-    aliotObj.run();
+  // Start connection process and listen for events
+  aliotObj.run();
 
-    lastMillis = millis(); // Set lastMillis to current millis
+  lastMillis = millis(); // Set lastMillis to current millis
 
-    // Add callback for valve 1
-    aliotObj.onActionRecv(valves[0].actionId, callbackValve1);  // Corrected actionID to actionId
+  // Add callback for valve 1
+  aliotObj.onActionRecv(valves[0].actionId,
+                        callbackValve1); // Corrected actionID to actionId
 
-    // Add callback for valve 2
-    aliotObj.onActionRecv(valves[1].actionId, callbackValve2);  // Corrected actionID to actionId
+  // Add callback for valve 2
+  aliotObj.onActionRecv(valves[1].actionId,
+                        callbackValve2); // Corrected actionID to actionId
 
-    // Add callback for valve 3
-    aliotObj.onActionRecv(valves[2].actionId, callbackValve3);  // Corrected actionID to actionId
+  // Add callback for valve 3
+  aliotObj.onActionRecv(valves[2].actionId,
+                        callbackValve3); // Corrected actionID to actionId
 
-    // Add callback for valve 4
-    aliotObj.onActionRecv(valves[3].actionId, callbackValve4);  // Corrected actionID to actionId
+  // Add callback for valve 4
+  aliotObj.onActionRecv(valves[3].actionId,
+                        callbackValve4); // Corrected actionID to actionId
 
-    // Add callback for valve 5
-    aliotObj.onActionRecv(valves[4].actionId, callbackValve5);  // Corrected actionID to actionId
+  // Add callback for valve 5
+  aliotObj.onActionRecv(valves[4].actionId,
+                        callbackValve5); // Corrected actionID to actionId
 
-    // Setup valve pin
-    for (int i = 0; i < sizeof(valves) / sizeof(valves[0]); i++) {
-        pinMode(valves[i].pin, OUTPUT);
-    }
+  // Add callback for remote wifi credential updates
+  aliotObj.onActionRecv("wifi_config", callbackWifiConfig);
 
-    Serial.println("Setup done");
+  // Setup valve pin
+  for (int i = 0; i < sizeof(valves) / sizeof(valves[0]); i++) {
+    pinMode(valves[i].pin, OUTPUT);
+  }
+
+  Serial.println("Setup done");
 }
 
 void loop() {
-    aliotObj.loop();
+  aliotObj.loop();
 
-    // Wait for delayMillis to make sure the connection is established
-    if (millis() - lastMillis > delayMillis && !isSetupDone) {
-        // Get valve state from ALIVEcode
-        updateValveFromServer();
+  // Wait for delayMillis to make sure the connection is established
+  if (millis() - lastMillis > delayMillis && !isSetupDone) {
+    // Get valve state from ALIVEcode
+    updateValveFromServer();
 
-        isSetupDone = true;
-    }
+    isSetupDone = true;
+  }
 
-    // Nothing to do here
+  // Nothing to do here
 }
